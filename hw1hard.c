d1969d1463a61f81448a5ea753762e6aedb0060e
zzxzhh <zz2324@columbia.edu>
2014-07-01 15:11:39 -0400

    homework1hard

---
+++ hw1hard.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(int argc, const char *argv[])
+{
+	int n,i,s,t;
+	int a[1000];
+	printf("Please enter an unsigned integer\n");
+	scanf("%d",&n);
+	while (n>=1) {
+		a[i] = n % 2;
+		if (n!= 1){
+		n = n / 2;
+		i++;
+		}`
+		else break;
+	}
+	t = 1;
+	while (i>=0){
+		s = s + t * a[i];
+		t = t * 2;
+		i = i - 1;
+	}
+	printf("%d\n",s);
+	return 0;
+
